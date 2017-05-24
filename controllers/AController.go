18ffd5df44162d0c6326de171ee05ec5b3eaa78c
Tinlone <zhaojl@wenduing.com>
2017-05-24 09:38:13 +0800

    Learn Custom Controller

---
+++ controllers/AController.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+)
+
+type AController struct {
+	beego.Controller
+}
+
+func (this *AController) Prepare() {
+
+}
+
+type Book struct {
+	Name   string `form:"name"`
+	Price  string `form:"price"`
+	Number string `form:"number"`
+}
+
+func (book *Book) toString() string {
+	return "Book:{name=" + book.Name + ",price=" + book.Price + ",number=" + book.Number + "}\n"
+}
+
+func (this *AController) Get() {
+	var s string
+	if err := this.Ctx.Input.Bind(&s, "s"); err != nil {
+		log.Println("get s error:", err)
+		this.Ctx.WriteString(string(s) + "\n")
+	} else {
+		log.Println("get [s] = ", s)
+		this.Ctx.WriteString(string(s) + "\n")
+	}
+	this.Ctx.WriteString("This is a interface!\n")
+}
+
+func (this *AController) Post() {
+	var book Book
+	json.Unmarshal(this.Ctx.Input.RequestBody, &book)
+	this.Ctx.WriteString(book.toString())
+	logs.Info(book.toString())
+}
--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"firstDemo/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/a", &controllers.AController{})
 }
